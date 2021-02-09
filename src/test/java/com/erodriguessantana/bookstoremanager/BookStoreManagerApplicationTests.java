565d965729fcc4d6e2915387f3d39bd708df634f
eort <eduardo.santana@gft.com>
2021-02-09 09:20:26 -0300

    Alteração de nome da classe principal para o padrão CamelCase

--- src/test/java/com/erodriguessantana/bookstoremanager/BookStoreManagerApplicationTests.java
+++ src/test/java/com/erodriguessantana/bookstoremanager/BookStoreManagerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookstoremanagerApplicationTests {
+class BookStoreManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
