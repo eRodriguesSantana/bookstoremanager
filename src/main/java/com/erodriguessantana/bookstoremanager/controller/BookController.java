56b59eec62e491b79a942b04d266203b971d2116
eort <eduardo.santana@gft.com>
2021-02-09 09:44:54 -0300

    Refatoração da classe BookController

--- src/main/java/com/erodriguessantana/bookstoremanager/controller/BookController.java
+++ src/main/java/com/erodriguessantana/bookstoremanager/controller/BookController.java
@@ -1,14 +1,8 @@
 package com.erodriguessantana.bookstoremanager.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.erodriguessantana.bookstoremanager.dto.BookDTO;
-import com.erodriguessantana.bookstoremanager.dto.MessageResponseDTO;
 import com.erodriguessantana.bookstoremanager.service.BookService;
 
 @RestController
@@ -16,15 +10,9 @@ import com.erodriguessantana.bookstoremanager.service.BookService;
 public class BookController {
 
 	private BookService bookService;
-	
-	//@Autowired
+
 	public BookController(BookService bookService) {
 		this.bookService = bookService;
-	}
-	
-	@PostMapping
-	public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
-		return bookService.create(bookDTO);
 	}
 
 }
