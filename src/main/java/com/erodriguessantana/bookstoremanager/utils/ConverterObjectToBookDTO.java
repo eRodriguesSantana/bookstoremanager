16ff493204ada3861ea28d7a9b8658fa0dcce5e1
eort <eduardo.santana@gft.com>
2021-02-19 12:20:52 -0300

    Criação de classe de conversão de Entidade para DTO

--- src/main/java/com/erodriguessantana/bookstoremanager/utils/ConverterObjectToBookDTO.java
+++ src/main/java/com/erodriguessantana/bookstoremanager/utils/ConverterObjectToBookDTO.java
@@ -1,26 +1,11 @@
 package com.erodriguessantana.bookstoremanager.utils;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.erodriguessantana.bookstoremanager.dto.BookDTO;
 import com.erodriguessantana.bookstoremanager.entity.Book;
-import com.erodriguessantana.bookstoremanager.repository.BookRepository;
 
-public class ConverterToBookDTO {
+public class ConverterObjectToBookDTO {
 
-	private BookRepository bookRepository;
-
-	public ConverterToBookDTO(BookRepository bookRepository) {
-		this.bookRepository = bookRepository;
-	}
-
-	public List<BookDTO> getBooks() {
-		return ((List<Book>) bookRepository.findAll()).stream().map(this::converterToBookDTO)
-				.collect(Collectors.toList());
-	}
-
-	private BookDTO converterToBookDTO(Book book) {
+	public BookDTO converterObjectToBookDTO(Book book) {
 		BookDTO bookDTO = new BookDTO();
 		bookDTO.setId(book.getId());
 		bookDTO.setName(book.getName());
