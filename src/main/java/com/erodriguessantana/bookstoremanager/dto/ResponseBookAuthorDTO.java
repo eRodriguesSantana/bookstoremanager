e76666f6401e5e8829a3779c0d43bcbef0349db4
eort <eduardo.santana@gft.com>
2021-02-19 14:06:33 -0300

    Classe DTO para exibição de Book e Author na mesma resposta

--- src/main/java/com/erodriguessantana/bookstoremanager/dto/ResponseBookAuthorDTO.java
+++ src/main/java/com/erodriguessantana/bookstoremanager/dto/ResponseBookAuthorDTO.java
@@ -1,25 +1,12 @@
 package com.erodriguessantana.bookstoremanager.dto;
 
-import javax.validation.constraints.NotNull;
-
 import com.erodriguessantana.bookstoremanager.entity.Author;
 import com.erodriguessantana.bookstoremanager.entity.Book;
 
 public class ResponseBookAuthorDTO {
 
-	@NotNull
 	private Book book;
-
-	@NotNull
 	private Author author;
-
-	public ResponseBookAuthorDTO() {
-	}
-
-	public ResponseBookAuthorDTO(Book book, Author author) {
-		this.book = book;
-		this.author = author;
-	}
 
 	public Book getBook() {
 		return book;
