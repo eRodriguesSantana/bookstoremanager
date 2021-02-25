97275a4a4ddfdab38c3321535e8d573ec4834ca7
eort <eduardo.santana@gft.com>
2021-02-25 15:34:18 -0300

    Tratamento de exception no método POST

--- src/main/java/com/erodriguessantana/bookstoremanager/controller/BookController.java
+++ src/main/java/com/erodriguessantana/bookstoremanager/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.erodriguessantana.bookstoremanager.controller;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,51 +25,55 @@ public class BookController {
 	public BookController(BookService bookService) {
 		this.bookService = bookService;
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<?> findBookById(@PathVariable Long id) {
 		BookDTO bookDtoId = bookService.findBookById(id);
-		if(bookDtoId != null)
+		if (bookDtoId != null)
 			return new ResponseEntity<>(bookDtoId, HttpStatus.OK);
 		return new ResponseEntity<>("ID do Book informado não existe na base de dados.", HttpStatus.NOT_FOUND);
 	}
-	
+
 	@GetMapping("/bookwithauthor/{id}")
 	public ResponseEntity<?> findBookAndAuthorById(@PathVariable Long id) {
 		ResponseBookAuthorDTO bookAuthorId = bookService.findByBookAuthorByID(id);
-		if(bookAuthorId != null)
+		if (bookAuthorId != null)
 			return new ResponseEntity<>(bookAuthorId, HttpStatus.OK);
 		return new ResponseEntity<>("ID do Book informado não existe na base de dados.", HttpStatus.NOT_FOUND);
 	}
-	
-	@PostMapping
-    public ResponseEntity<?> save(@RequestBody BookDTO bookDTO) {	
-        Book bookSaved = bookService.save(bookDTO);
-        
-		return bookSaved != null 
-				? new ResponseEntity<>(bookSaved, HttpStatus.CREATED) 
-				: new ResponseEntity<>("ID do Author informado não existe na base de dados.", HttpStatus.NOT_FOUND);
-    }
-	
+
+	@PostMapping("/create")
+	public ResponseEntity<?> save(@RequestBody BookDTO bookDTO) {
+		try {
+			Book bookSaved = bookService.save(bookDTO);
+
+			return bookSaved != null 
+					? new ResponseEntity<>(bookSaved, HttpStatus.CREATED)
+					: new ResponseEntity<>("ID do Author informado não existe na base de dados.", HttpStatus.NOT_FOUND);
+		} catch (DataIntegrityViolationException ex) {
+			return new ResponseEntity<>("Já existe um Book com esse nome: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 	@PutMapping("/{id}")
 	public ResponseEntity<?> update(@RequestBody BookDTO bookDTO, @PathVariable Long id) {
-		
-		if(bookService.findBookById(id) != null) {
+
+		if (bookService.findBookById(id) != null) {
 			Book bookSaved = bookService.save(bookDTO);
-			
-	        if(bookSaved != null) {
-	        	bookDTO.setId(id);        	
-	        	bookSaved.setName(bookDTO.getName());
-	        	bookSaved.setPages(bookDTO.getPages());
-	        	bookSaved.setChapters(bookDTO.getChapters());
-	        	bookSaved.setIsbn(bookDTO.getIsbn());
-	        	bookSaved.setPublisherName(bookDTO.getPublisherName());
-	        	bookSaved.setIdAuthor(bookDTO.getIdAuthor());
-	        	
-	        	return new ResponseEntity<>(bookSaved, HttpStatus.CREATED);
-	        }
+
+			if (bookSaved != null) {
+				bookDTO.setId(id);
+				bookSaved.setName(bookDTO.getName());
+				bookSaved.setPages(bookDTO.getPages());
+				bookSaved.setChapters(bookDTO.getChapters());
+				bookSaved.setIsbn(bookDTO.getIsbn());
+				bookSaved.setPublisherName(bookDTO.getPublisherName());
+				bookSaved.setIdAuthor(bookDTO.getIdAuthor());
+
+				return new ResponseEntity<>(bookSaved, HttpStatus.CREATED);
+			}
 		}
-        return new ResponseEntity<>("ID informado para atualização não existe na base de dados.", HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>("ID informado para atualização não existe na base de dados.", HttpStatus.NOT_FOUND);
 	}
-	
+
 }
