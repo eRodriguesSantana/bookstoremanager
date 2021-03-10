cd1a35a620f13b104d7ed20e223c229a3f64f1d7
eort <eduardo.santana@gft.com>
2021-03-10 17:49:35 -0300

    Adição da price na tabela book

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO author (id, name, age, day_birth, month_birth, year_birth) VALUES (1
 INSERT INTO author (id, name, age, day_birth, month_birth, year_birth) VALUES (2, 'José Lins do Rego', 80, 13, 04, 1880);
 INSERT INTO author (id, name, age, day_birth, month_birth, year_birth) VALUES (3, 'Machado de Assis', 90, 27, 02, 1989);
 
-INSERT INTO book (id, name, pages, chapters, isbn, book_type, publisher_name, id_author) VALUES (1, 'O Primo Basilio', 200, 8, '2-2565-522', 'LITERATURA', 'Editora Abril', 1);
-INSERT INTO book (id, name, pages, chapters, isbn, book_type, publisher_name, id_author) VALUES (2, 'Fogo Morto', 200, 8, '3-2565-523', 'LITERATURA', 'Editora Globo', 2);
-INSERT INTO book (id, name, pages, chapters, isbn, book_type, publisher_name, id_author) VALUES (3, 'Memórias Póstumas', 200, 8, '6-2565-526', 'LITERATURA', 'Editora Livre', 3);
+INSERT INTO book (id, name, price, pages, chapters, isbn, book_type, publisher_name, id_author) VALUES (1, 'O Primo Basilio', 60, 200, 8, '2-2565-522', 'LITERATURA', 'Editora Abril', 1);
+INSERT INTO book (id, name, price, pages, chapters, isbn, book_type, publisher_name, id_author) VALUES (2, 'Fogo Morto', 70, 200, 8, '3-2565-523', 'TERROR', 'Editora Globo', 2);
+INSERT INTO book (id, name, price, pages, chapters, isbn, book_type, publisher_name, id_author) VALUES (3, 'Memórias Póstumas', 80, 200, 8, '6-2565-526', 'BIOGRAFIA', 'Editora Livre', 3);
